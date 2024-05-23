8094445b0b9ef8cd1192917ba5e0accb8758e821
Pranav Pratyush <99145117+Champ2979@users.noreply.github.com>
2024-05-23 12:42:25 +0530

    Update K-Palindrome.java

--- GFG POTD/K-Palindrome.java
+++ GFG POTD/K-Palindrome.java
@@ -1,14 +1,11 @@
 class Solution {
     public int kPalindrome(String str, int n, int k) {
-         // Create a DP table to store results of subproblems
         int[][] dp = new int[n][n];
         
-        // Initialize the table
+       
         for (int i = 0; i < n; i++) {
             dp[i][i] = 0; // A single character is already a palindrome
         }
-        
-        // Fill the DP table
         for (int length = 2; length <= n; length++) {
             for (int i = 0; i <= n - length; i++) {
                 int j = i + length - 1;
@@ -19,14 +16,12 @@ class Solution {
                 }
             }
         }
-        
-        // The result for the whole string is in dp[0][n-1]
         return dp[0][n - 1] <= k ? 1 : 0;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.kPalindrome("abcdecba", 8, 1)); // Output: 1
-        System.out.println(solution.kPalindrome("abcdefcba", 9, 1)); // Output: 0
+        System.out.println(solution.kPalindrome("abcdecba", 8, 1)); 
+        System.out.println(solution.kPalindrome("abcdefcba", 9, 1)); 
     }
 }
