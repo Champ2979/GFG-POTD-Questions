71e07072d3c7880673d543740d6137924e073dda
Pranav Pratyush <99145117+Champ2979@users.noreply.github.com>
2024-06-23 22:05:09 +0530

    Add files via upload

---
+++ GFG POTD/PrintBracketNumber.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+class PrintBracketNumber {
+    ArrayList<Integer> bracketNumbers(String str) {
+        ArrayList<Integer> result = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        int counter = 1;
+
+        for (char ch : str.toCharArray()) {
+            if (ch == '(') {
+                stack.push(counter);
+                result.add(counter);
+                counter++;
+            } else if (ch == ')') {
+                result.add(stack.pop());
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String str1 = "(aa(bdc))p(dee)";
+        String str2 = "(((()(";
+
+        System.out.println(sol.bracketNumbers(str1)); // Output: [1, 2, 2, 1, 3, 3]
+        System.out.println(sol.bracketNumbers(str2)); // Output: [1, 2, 3, 4, 4, 5]
+    }
+}
+
